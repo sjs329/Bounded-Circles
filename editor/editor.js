62336c12c8d76ff2ada020d656552691dfdc0fe5
Scott Schlacter <scottschlacter@Scotts-MacBook-Pro.local>
2016-09-23 22:53:06 -0400

    Adding line-drawing to level editor (shift-click to define lines)

--- editor/editor.js
+++ editor/editor.js
@@ -2,13 +2,55 @@ var Editor = (function(editor) {
   editor.start = function() {
       editor.canvas = document.getElementById('bounded_circles')
       editor.screen = editor.canvas.getContext('2d');
-      var font = { style: "50px Verdana", align: "center", color: "black" };
-      editor.dimensions = { x: editor.screen.canvas.width,
-                            y: editor.screen.canvas.height - 50 };
-      var testing = new editor.Text("Hello World!",
-              {x: editor.dimensions.x/2, y: editor.dimensions.y/4+10}, 0, font);
-      testing.draw(editor.screen);
-      var mouser = new Mouser(editor.canvas, editor.screen);
+      // var font = { style: "50px Verdana", align: "center", color: "black" };
+      // editor.dimensions = { x: editor.screen.canvas.width,
+      //                       y: editor.screen.canvas.height - 50 };
+      // var testing = new editor.Text("Hello World!",
+      //         {x: editor.dimensions.x/2, y: editor.dimensions.y/4+10}, 0, font);
+      // testing.draw(editor.screen);
+      editor.dimensions = { x: editor.screen.canvas.width, y: editor.screen.canvas.height - 50 };
+      
+      //draw border
+      editor.non_game_lines = []
+      //outer border
+      editor.non_game_lines.push({ pt1: { x: 0, y: 0 }, pt2: { x: 0, y: editor.screen.canvas.height } })
+      editor.non_game_lines.push({ pt1: { x: 0, y: editor.screen.canvas.height }, pt2: { x: editor.screen.canvas.width, y: editor.screen.canvas.height } })
+      editor.non_game_lines.push({ pt1: { x: editor.screen.canvas.width, y: editor.screen.canvas.height }, pt2: { x: editor.screen.canvas.width, y: 0 } })
+      editor.non_game_lines.push({ pt1: { x: editor.screen.canvas.width, y: 0 }, pt2: { x: 0, y: 0 } })
+
+      //line above weapon info text
+      editor.non_game_lines.push({ pt1: { x: 0, y: editor.dimensions.y}, pt2: {x:editor.dimensions.x, y:editor.dimensions.y} })
+
+
+      for (var i=0; i<editor.non_game_lines.length; i++) {
+        editor.drawLine(editor.non_game_lines[i])
+      }
+
+      var mouser = new Mouser(editor); //.canvas, editor.screen);
+  };
+
+  editor.drawLine = function(line) {
+    editor.screen.beginPath();
+    editor.screen.lineWidth = 1.5;
+    editor.screen.moveTo(line.pt1.x, line.pt1.y);
+    editor.screen.lineTo(line.pt2.x, line.pt2.y);
+    editor.screen.closePath();
+    editor.screen.strokeStyle = "black";
+    editor.screen.stroke();
+  };
+
+  editor.drawCircle = function(circle) {
+    editor.screen.beginPath();
+    editor.screen.strokeStyle = "black";
+    editor.screen.arc(circle.center.x, circle.center.y, circle.radius, 0, Math.PI * 2, true);
+    editor.screen.closePath();
+    editor.screen.stroke();
+
+    editor.screen.beginPath();
+    editor.screen.arc(circle.center.x, circle.center.y, circle.radius, 0, Math.PI * 2, true);
+    editor.screen.closePath();
+    editor.screen.fillStyle = "green";
+    editor.screen.fill();
   };
 
   window.addEventListener('load', editor.start);
--- editor/mouser.js
+++ editor/mouser.js
@@ -1,50 +1,65 @@
-var Mouser = function(canvas, screen) {
-
-    // coordinates of upper left corner of screen, needed to remove the offset.
-    var rect = canvas.getBoundingClientRect();
-    var startingXCoord = rect.left;
-    var startingYCoord = rect.top; 
-
+var Mouser = function(editor) {
     // for drawing circles.
     var radius = 10;
 
     // for recording clicks, to be written to file.
-    lineXCoords = [];
-    lineYCoords = [];
-    circleXCoords = [];
-    circleYCoords = [];
-
+    circles = [];
+    lines = [];
+    drawingLine = false;
 
     function getCoords(evt) {
-        if (evt.shiftKey) {
+        var rect = editor.canvas.getBoundingClientRect();
+        if (drawingLine) {
+            drawingLine = false;
+        }
+        else if (evt.shiftKey) {
             // shift=left click: draw line
-            lineXCoords.push(evt.clientX - startingXCoord);
-            lineYCoords.push(evt.clientY - startingYCoord);
-            alert('shift-click' + evt.clientX + " " + evt.clientY); 
+            var line =  {    
+                            pt1: {x: evt.clientX - rect.left, y: evt.clientY - rect.top}, 
+                            pt2: {x: evt.clientX - rect.left+1, y: evt.clientY - rect.top+1}
+                        }
+            lines.push(line)
+            drawingLine = true;
         }
         else {
             // left click: circle
-            circleXCoords.push(evt.clientX - startingXCoord);
-            circleYCoords.push(evt.clientY - startingYCoord);
-            //alert('click' + evt.clientX + " " + evt.clientY); 
-            screen.beginPath();
-            screen.strokeStyle = "black";
-            screen.arc(evt.clientX - startingXCoord,
-                       evt.clientY - startingYCoord,
-                       radius, 0, Math.PI * 2, true);
-            screen.closePath();
-            screen.stroke();
+            var circle = {  center: 
+                                {   x: evt.clientX - rect.left,
+                                    y: evt.clientY - rect.top
+                                },
+                            radius: 10
+                         }
+            circles.push(circle)
         }
         
     };
     window.addEventListener('click', getCoords);
 
+    function getPosition(evt) {
+        if (drawingLine) {
+            var rect = editor.canvas.getBoundingClientRect();
+            lines[lines.length-1].pt2.x = evt.clientX-rect.left;
+            lines[lines.length-1].pt2.y = evt.clientY-rect.top;
+        }
+    };
+    window.addEventListener('mousemove', getPosition);
+
     function saveLevel() {
         // Write circles.
         var file = "circles ["
-        for (i = 0; i < circleXCoords.length; i++) {
-            file += circleXCoords[i] + " " + circleYCoords[i] + " 0 0";
-            if (i < circleXCoords.length - 1) {
+        for (var i = 0; i < circles.length; i++) {
+            file += circles[i].center.x + " " + circles[i].center.y + " 0 0";
+            if (i < circles.length - 1) {
+                file += ","
+            }
+        }
+        file += "]\n"
+
+        // Write lines
+        file += "lines ["
+        for (var i=0; i<lines.length; i++) {
+            file += lines[i].pt1.x + " " + lines[i].pt1.y + " " + lines[i].pt2.x + " " + lines[i].pt2.y;
+            if (i < lines.length - 1) {
                 file += ","
             }
         }
@@ -53,4 +68,24 @@ var Mouser = function(canvas, screen) {
     }
     document.getElementById('saveButton').addEventListener('click',
             saveLevel);
+
+    function tick() {
+        // Clear away the drawing from the previous tick.
+        editor.screen.clearRect(1, 1, editor.dimensions.x-2, editor.dimensions.y-2); //but don't clear the border lines
+        
+        //draw lines
+        for (var i=0; i<lines.length; i++) {
+            // console.log("Drawing line",lines[i])
+            editor.drawLine(lines[i])
+        }
+
+        //draw circles
+        for (var i=0; i<circles.length; i++) {
+            editor.drawCircle(circles[i])
+        }
+        
+        // Queue up the next call to tick with the browser.
+        requestAnimationFrame(tick);
+    }
+    tick();
 }
